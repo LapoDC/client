294a9d59b5e55de11b2c90dc531147e78c39933c
lai2-24 <lai2-24@itismeucci.com>
2024-10-11 10:44:24 +0200

    terzo commit

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -10,20 +10,29 @@ import java.net.UnknownHostException;
 public class Main {
     public static void main(String[] args) throws UnknownHostException, IOException {
         System.out.println("il Client è partito");
-        Socket s = new Socket("localhost", 63000);
+        Socket s = new Socket("localhost", 9000);
         System.out.println("il client si è collegato");
-        BufferedReader in= new BufferedReader(new InputStreamReader(s.getInputStream()));
+        BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
         DataOutputStream out = new DataOutputStream(s.getOutputStream());
-        BufferedReader inTastiera= new BufferedReader(new InputStreamReader(System.in));
-        
-        String nomeScelto=inTastiera.readLine();
-        out.writeBytes(nomeScelto+"\n");
-
-        String stringaRicevuta=in.readLine();
-        System.out.println("La dtringa ricevuta"+ stringaRicevuta);
+        BufferedReader inTastiera = new BufferedReader(new InputStreamReader(System.in));
+       
         System.out.println("inserisci il tuo nome");
 
         
+       
+        do {
+            
+            String nomeScelto = inTastiera.readLine();
+            out.writeBytes(nomeScelto + "\n");
+            String stringaRicevuta=in.readLine();
+
+            if (stringaRicevuta.equals("!"))
+                break;
+ 
+            System.out.println("La stringa ricevuta " + stringaRicevuta);
+          
+
+        } while (true);
         s.close();
     }
 }
